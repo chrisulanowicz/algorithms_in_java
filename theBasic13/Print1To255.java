958b698f1aa8b562ce8586ad3bd9d764caeaaac0
Chris Ulanowicz <src3collector@gmail.com>
2017-07-31 09:25:05 -0500

    added print 1 to 255 in basic 13

---
+++ theBasic13/Print1To255.java
@@ -0,0 +1,14 @@
+// Function that simply prints the numbers 1 - 255
+// Print all the integers from 1 to 255.
+
+public class Print1To255 {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<256;i++) { // create a basic for loop to iterate through each number
+			System.out.println(i); // and print it out
+		}
+
+	}
+
+}
